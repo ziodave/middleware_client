fe2a348c63de4c691b22e898c3dc9b75efb1f8f5
David Riccitelli <david@wordlift.io>
2024-05-03 08:18:52 +0200

    only entities

--- middleware_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from middleware_client.api.autocomplete_api import AutocompleteApi
-from middleware_client.api.dataset_api import DatasetApi
-from middleware_client.api.entities_api import EntitiesApi
-
